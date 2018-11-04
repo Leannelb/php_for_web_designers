0213f1843afd95798014724d23742123fdfac89b
Lillycode <leannelaceybyrne@outlook.com>
2018-11-03 19:59:54 -0700

    the spelling of list was incorrect to see error messages

--- 02_02/include.php
+++ 02_02/include.php
@@ -10,6 +10,7 @@
 <h1>Carry On with include</h1>
 <p>This paragraph is in the original HTML, and it should be followed by an unordered list which comes from an include file.</p>
 <?php include ('includes/list.php'); ?>
+<p>the list / lists is important. this was changed in order to see the error messages for includes/requires. List is correct</p>
 <p>with an imclude file as above, if the file can't be found it will still continue down the page. Therefore the line seen below, will still render as the script carries on.</p>
 <p>The final paragraph is in the original HTML, and should be displayed even if the external file can't be found.</p>
 </body>
