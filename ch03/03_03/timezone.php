aa32e5bb09b0f36ca95bc63442916583016482cb
Lillycode <leannelaceybyrne@outlook.com>
2018-11-04 15:42:51 -0800

    using php date time to display times snd change timezones

--- ch03/03_03/timezone.php
+++ ch03/03_03/timezone.php
@@ -1,6 +1,9 @@
 <?php
 $day = date('l');
 $time = date('H:i');
+date_default_timezone_set('Europe/Malta');
+$day_HI = date('l');
+$time_HI = date('H:i')
 ?>
 <!doctype html>
 <html>
@@ -13,5 +16,7 @@ $time = date('H:i');
 <body>
 <h1>Changing the Time Zone</h1>
 <p>The local time is now <?php echo $time; ?> on <?php echo $day; ?>.</p>
+<p>The local time in Hawaii is now <?php echo $time_HI; ?> on <?php echo $day_HI; ?>.</p>
+
 </body>
 </html>
