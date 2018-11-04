c5df46ea813854c154609a1369a49206fb8a79e8
Lillycode <leannelaceybyrne@outlook.com>
2018-11-04 08:55:53 -0800

    restarted handsel and petal

--- hanselandpetal/includes/quick_links.php
+++
@@ -1,5 +0,0 @@
-<ul id="quick_links" class="reset menu">
-    <li><a href="#">My Account</a></li>
-    <li><a href="order.php">View My Order</a></li>
-    <li><a href="#">Customer Service</a></li>
-</ul>
