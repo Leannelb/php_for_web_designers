1b2bc8aeafc08dc8dcfc747b07715e6820ed988c
Lillycode <leannelaceybyrne@outlook.com>
2018-11-03 19:09:07 -0700

    server side inputs general info and use cases added

--- includes/external.php
+++ includes/external.php
@@ -1,3 +1,8 @@
 <p>This paragraph is in an external file.</p>
-<h2>Heading</h2>
+<h2>Common to store server side includes in own folder includes/</h2>
+<p>file extension is .php</p>
+<p>some people use .inc extensions but his is not reccomended</p>
+<p>Can include anything you'd have in a normal webpage html,php etc.</p>
+<p>Used to serve common peices of code to multiple places</p>
+<p>notice there is no markup</p>
 <p>using server side includes is a real time saver</p>
