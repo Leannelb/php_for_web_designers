47966388fdac167c760f5ecb2662da0a1444ac60
Lillycode <leannelaceybyrne@outlook.com>
2018-11-04 20:50:49 -0800

    error message saved : echoing an array

--- ch04/04_05/join_array.php
+++ ch04/04_05/join_array.php
@@ -14,5 +14,12 @@ $flowers[] = 'irises';
 <h1>Joining an Array with implode()</h1>
 <p><?php echo $flowers; ?></p>
 <!-- Trying to echo an array gives errors -->
+<!-- error is:
+
+( ! ) Notice: Array to string conversion in /Applications/MAMP/htdocs/exercises/ch02/02_01/ch04/04_05/join_array.php on line 15
+Call Stack
+#	Time	Memory	Function	Location
+1	0.0004	401720	{main}( )	.../join_array.php:0
+Array -->
 </body>
 </html>
