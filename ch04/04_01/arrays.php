36d458937f1dc9ebaca652e947eb61af077cfcc9
Lillycode <leannelaceybyrne@outlook.com>
2018-11-04 19:00:45 -0800

    adding to end of php array

--- ch04/04_01/arrays.php
+++ ch04/04_01/arrays.php
@@ -1,7 +1,9 @@
 <?php $flowers = array('tulips', 'roses', 'daffodills', 'orchids');?>
 <?php $flowers2 = ['tulips', 'roses', 'daffodills', 'orchids'];?>
 <!-- 2 ways to define arrays above -->
-
+<?php $flowers[] = 'irisis';?> 
+<?php $flowers2[] = 'irisis';?> 
+<!-- To add values to an array do the above -->
 <!doctype html>
 <html>
 <head>
