f3da98752f8c528fb6dddd937ce8b06f53148331
Lillycode <leannelaceybyrne@outlook.com>
2018-11-05 21:36:32 +0100

    accessing items in an associative array and printing to screen

--- ch04/04_07/associative.php
+++ ch04/04_07/associative.php
@@ -1,6 +1,12 @@
 <?php
 $flowers = array('tulips', 'roses', 'daffodils', 'orchids', 'daisies');
 $flowers[] = 'irises';
+$features = array(
+    'winter' => 'Beautiful arrangements for any occasion',
+    'spring' => 'It must be spring! The daffodills are here',
+    'summer' => "It's summer, and we're in the pink",
+    'autumn' => "Summer's over but our flowers are still a riot of colors"
+);
 ?>
 <!doctype html>
 <html>
@@ -12,6 +18,10 @@ $flowers[] = 'irises';
 
 <body>
 <h1>Labeling Array Elements</h1>
+<p><?php echo "The tagline for spring is: {$features['spring']}";?></p>
+<!-- IF you want a double quote in your sting you must include:  {$features['spring']} brackets. Else error  -->
+<!-- This prints the following to the screen: 'spring' => 'It must be spring! The daffodills are here', -->
+
 <pre>
 <?php print_r($flowers); ?>
 </pre>
